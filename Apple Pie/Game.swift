7aeda6e46f3ca4ce71712d8dce55e84d3156e0ef
Sergey Mityanin <s.mityanin@gmail.com>
2020-11-06 22:33:36 +0300

    Added game model

---
+++ Apple Pie/Game.swift
@@ -0,0 +1,11 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Сергей on 06.11.2020.
+//
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+}
--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - IB Outlets
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet var letterButtons: [UIButton]!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    // MARK: - Properties
+    var currentGame: Game!
+    let incorrectMovesAllowed = 7
+    var listOfWords = [
+        "Малина",
+        "Полина",
+        "Калина",
+        "Долина",
+    ]
+    var totalLosses = 0
+    var totalWins = 0
+    
+    // MARK: - Methods
+    func newRound() {
+        currentGame = Game(word: listOfWords.removeFirst(), incorrectMovesRemaining: incorrectMovesAllowed)
+        updateUI()
+    }
+    
+    func updateUI() {
+        let movesRemaining = currentGame.incorrectMovesRemaining
+        let imageName = "Tree_\(movesRemaining < 8 ? movesRemaining : 7).pdf"
+        treeImageView.image = UIImage(named: imageName)
+        scoreLabel.text = "Выиграно: \(totalWins). Проиграно: \(totalLosses)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        newRound()
     }
-
-
+    
+    // MARK: - IB Actions
+    @IBAction func letterButtonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
+    
 }
 
